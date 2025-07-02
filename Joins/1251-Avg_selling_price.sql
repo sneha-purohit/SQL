a9e7daae7a59ee7889ed250f92d6374e0376f2c9
sneha-purohit <snehapurohit04@gmail.com>
2025-07-02 17:42:49 +0530

    Add files via upload

---
+++ Joins/1251-Avg_selling_price.sql
@@ -0,0 +1,32 @@
+# https://leetcode.com/problems/average-selling-price/description/
+
+create database selling;
+use selling;
+
+
+Create table If Not Exists Prices (product_id int, start_date date, end_date date, price int);
+Create table If Not Exists UnitsSold (product_id int, purchase_date date, units int);
+Truncate table Prices;
+insert into Prices (product_id, start_date, end_date, price) values ('1', '2019-02-17', '2019-02-28', '5');
+insert into Prices (product_id, start_date, end_date, price) values ('1', '2019-03-01', '2019-03-22', '20');
+insert into Prices (product_id, start_date, end_date, price) values ('2', '2019-02-01', '2019-02-20', '15');
+insert into Prices (product_id, start_date, end_date, price) values ('2', '2019-02-21', '2019-03-31', '30');
+Truncate table UnitsSold;
+insert into UnitsSold (product_id, purchase_date, units) values ('1', '2019-02-25', '100');
+insert into UnitsSold (product_id, purchase_date, units) values ('1', '2019-03-01', '15');
+insert into UnitsSold (product_id, purchase_date, units) values ('2', '2019-02-10', '200');
+insert into UnitsSold (product_id, purchase_date, units) values ('2', '2019-03-22', '30');
+
+select p.product_id, ifnull(round(sum(p.price*u.units)/sum(u.units),2),0) as
+average_price
+from Prices p
+left join UnitsSold u
+on p.product_id = u.product_id
+and u.purchase_date >= p.start_date
+and u.purchase_date <= p.end_date
+group by p.product_id;
+
+
+
+
+
