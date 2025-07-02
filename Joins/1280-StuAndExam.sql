a1fd5a75453f35f2b013c239acc7ad5fd5de0329
sneha-purohit <snehapurohit04@gmail.com>
2025-07-02 17:43:31 +0530

    Add files via upload

---
+++ Joins/1280-StuAndExam.sql
@@ -0,0 +1,45 @@
+# https://leetcode.com/problems/students-and-examinations
+create database stu_exam;
+use stu_exam;
+
+Create table If Not Exists Students (student_id int, student_name varchar(20));
+Create table If Not Exists Subjects (subject_name varchar(20));
+Create table If Not Exists Examinations (student_id int, subject_name varchar(20));
+Truncate table Students;
+insert into Students (student_id, student_name) values ('1', 'Alice');
+insert into Students (student_id, student_name) values ('2', 'Bob');
+insert into Students (student_id, student_name) values ('13', 'John');
+insert into Students (student_id, student_name) values ('6', 'Alex');
+Truncate table Subjects;
+insert into Subjects (subject_name) values ('Math');
+insert into Subjects (subject_name) values ('Physics');
+insert into Subjects (subject_name) values ('Programming');
+Truncate table Examinations;
+insert into Examinations (student_id, subject_name) values ('1', 'Math');
+insert into Examinations (student_id, subject_name) values ('1', 'Physics');
+insert into Examinations (student_id, subject_name) values ('1', 'Programming');
+insert into Examinations (student_id, subject_name) values ('2', 'Programming');
+insert into Examinations (student_id, subject_name) values ('1', 'Physics');
+insert into Examinations (student_id, subject_name) values ('1', 'Math');
+insert into Examinations (student_id, subject_name) values ('13', 'Math');
+insert into Examinations (student_id, subject_name) values ('13', 'Programming');
+insert into Examinations (student_id, subject_name) values ('13', 'Physics');
+insert into Examinations (student_id, subject_name) values ('2', 'Math');
+insert into Examinations (student_id, subject_name) values ('1', 'Math');
+
+select s.student_id , s.student_name , sub.subject_name ,
+count(e.subject_name) as attended_exams
+from Students as s
+cross join Subjects as sub
+left join Examinations as e
+on s.student_id = e.student_id and
+sub.subject_name = e.subject_name
+group by s.student_id , s.student_name , sub.subject_name
+order by s.student_id , sub.subject_name
+
+
+
+
+
+
+
