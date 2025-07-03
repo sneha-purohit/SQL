7ec8aafb87c6076caf131194f406174d0acef18f
sneha-purohit <snehapurohit04@gmail.com>
2025-07-03 23:31:43 +0530

    Add files via upload

---
+++ Clause/1211-QueriesQualityAND%.sql
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/queries-quality-and-percentage
+
+Create table If Not Exists Queries (query_name varchar(30), result varchar(50), position int, rating int);
+Truncate table Queries;
+insert into Queries (query_name, result, position, rating) values ('Dog', 'Golden Retriever', '1', '5');
+insert into Queries (query_name, result, position, rating) values ('Dog', 'German Shepherd', '2', '5');
+insert into Queries (query_name, result, position, rating) values ('Dog', 'Mule', '200', '1');
+insert into Queries (query_name, result, position, rating) values ('Cat', 'Shirazi', '5', '2');
+insert into Queries (query_name, result, position, rating) values ('Cat', 'Siamese', '3', '3');
+insert into Queries (query_name, result, position, rating) values ('Cat', 'Sphynx', '7', '4');
+
+select query_name, round(avg(rating/position), 2) as quality,
+round(avg(if(rating < 3, 1, 0))*100, 2) as poor_query_percentage
+from Queries
+group by query_name
