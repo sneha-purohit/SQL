dd84953be9ddc92e5c78c6e1e458373016340818
sneha-purohit <snehapurohit04@gmail.com>
2025-07-07 22:38:11 +0530

    Add files via upload

---
+++ Clause/Emp_earn_more_than_manager.sql
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/employees-earning-more-than-their-managers
+
+Create table If Not Exists Employee (id int, name varchar(255), salary int, managerId int);
+Truncate table Employee;
+insert into Employee (id, name, salary, managerId) values ('1', 'Joe', '70000', '3');
+insert into Employee (id, name, salary, managerId) values ('2', 'Henry', '80000', '4');
+insert into Employee (id, name, salary, managerId) values ('3', 'Sam', '60000', NULL);
+insert into Employee (id, name, salary, managerId) values ('4', 'Max', '90000', NULL);
+
+SELECT A.NAME AS EMPLOYEE
+FROM EMPLOYEE A LEFT JOIN EMPLOYEE B
+ON A.MANAGERID = B.ID
+WHERE A.SALARY > B.SALARY
