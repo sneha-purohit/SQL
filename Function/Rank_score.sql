3a6970408f26fd7cbea1a050cc1871840f9c251f
sneha-purohit <snehapurohit04@gmail.com>
2025-07-09 23:49:03 +0530

    Add files via upload

---
+++ Function/Rank_score.sql
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/rank-scores
+
+create database sco;
+use sco;
+
+Create table If Not Exists Scores (id int, score DECIMAL(3,2));
+Truncate table Scores;
+insert into Scores (id, score) values ('1', '3.5');
+insert into Scores (id, score) values ('2', '3.65');
+insert into Scores (id, score) values ('3', '4.0');
+insert into Scores (id, score) values ('4', '3.85');
+insert into Scores (id, score) values ('5', '4.0');
+insert into Scores (id, score) values ('6', '3.65');
+
+SELECT score,
+DENSE_RANK() OVER (ORDER BY score DESC) AS 'rank'
+FROM scores;
