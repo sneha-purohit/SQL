6e23fb3e27b815135dbe30b35cf13d35316ef830
sneha-purohit <snehapurohit04@gmail.com>
2025-07-12 22:25:38 +0530

    Add files via upload

---
+++ Clause/class_atleast_5_stu.sql
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/classes-with-at-least-5-students
+
+Create table If Not Exists Courses (student varchar(255), class varchar(255));
+Truncate table Courses;
+insert into Courses (student, class) values ('A', 'Math');
+insert into Courses (student, class) values ('B', 'English');
+insert into Courses (student, class) values ('C', 'Math');
+insert into Courses (student, class) values ('D', 'Biology');
+insert into Courses (student, class) values ('E', 'Math');
+insert into Courses (student, class) values ('F', 'Computer');
+insert into Courses (student, class) values ('G', 'Math');
+insert into Courses (student, class) values ('H', 'Math');
+insert into Courses (student, class) values ('I', 'Math');
+
+SELECT class
+FROM Courses
+GROUP BY class
+HAVING count(class) >= 5
