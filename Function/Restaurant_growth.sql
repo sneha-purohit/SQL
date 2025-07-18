d6d5d706b773a6671b049d459fcf3541146e5fde
sneha-purohit <snehapurohit04@gmail.com>
2025-07-18 23:47:34 +0530

    Add files via upload

---
+++ Function/Restaurant_growth.sql
@@ -0,0 +1,38 @@
+# https://leetcode.com/problems/restaurant-growth/
+
+create database custo;
+use custo;
+
+Create table If Not Exists Customer (customer_id int, name varchar(20), visited_on date, amount int);
+Truncate table Customer;
+insert into Customer (customer_id, name, visited_on, amount) values ('1', 'Jhon', '2019-01-01', '100');
+insert into Customer (customer_id, name, visited_on, amount) values ('2', 'Daniel', '2019-01-02', '110');
+insert into Customer (customer_id, name, visited_on, amount) values ('3', 'Jade', '2019-01-03', '120');
+insert into Customer (customer_id, name, visited_on, amount) values ('4', 'Khaled', '2019-01-04', '130');
+insert into Customer (customer_id, name, visited_on, amount) values ('5', 'Winston', '2019-01-05', '110');
+insert into Customer (customer_id, name, visited_on, amount) values ('6', 'Elvis', '2019-01-06', '140');
+insert into Customer (customer_id, name, visited_on, amount) values ('7', 'Anna', '2019-01-07', '150');
+insert into Customer (customer_id, name, visited_on, amount) values ('8', 'Maria', '2019-01-08', '80');
+insert into Customer (customer_id, name, visited_on, amount) values ('9', 'Jaze', '2019-01-09', '110');
+insert into Customer (customer_id, name, visited_on, amount) values ('1', 'Jhon', '2019-01-10', '130');
+insert into Customer (customer_id, name, visited_on, amount) values ('3', 'Jade', '2019-01-10', '150');
+
+select visited_on,
+( select sum(amount)
+  from customer
+  where visited_on between date_sub(c.visited_on, interval 6 day)
+  and c.visited_on
+) as amount,
+  Round(
+( select sum(amount)/7
+  from customer
+  where visited_on between date_sub(c.visited_on, interval 6 day)
+  and c.visited_on
+),2) as average_amount
+from Customer c
+where visited_on >= (
+    select date_add(min(visited_on), interval 6 day)
+    from Customer
+)
+group by visited_on
+order by visited_on;
